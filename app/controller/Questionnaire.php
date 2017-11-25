ff2d27b873767cf4b10cc37b89c4465c4d5276ce
Jeremy <1025024131@qq.com>
2017-11-25 11:41:41 +0800

    修改问卷星数据推送接收端代码-jeremy-2017年11月25日 11:41

--- app/controller/Questionnaire.php
+++ app/controller/Questionnaire.php
@@ -147,7 +147,7 @@ class Questionnaire extends Common
      * 导入问卷星
      */
     public function getPostData(){
-        $data = input('get.');
+        $data = input('post.');
         $now = time();
         file_put_contents('./public/data/questionnaire/'.$now.'.txt', $data);
     }
